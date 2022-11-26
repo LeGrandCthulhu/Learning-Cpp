b6f436f9c3c749708c6b356800691aa0bf511c26
LeGrandCthulhu <pierre.frebet@gmail.com>
2022-11-26 22:59:29 +0100

    Importation des fichiers

---
+++ Tests/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string monNom("Pierre");
+    cout << "Hello world!" << endl << "Je suis " << monNom << ". J'adore le C++, mais c'est complique." << endl;
+
+    return 0;
+}
